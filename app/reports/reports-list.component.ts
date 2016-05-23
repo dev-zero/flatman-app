1a65bfd9492a9f8d54690352d849c049a117e084
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-05-23 16:01:22 +0200

    webapp: reformat template in ReportsList

--- app/reports/reports-list.component.ts
+++ app/reports/reports-list.component.ts
@@ -4,25 +4,25 @@ import {OnActivate, Router, RouteSegment, RouteTree} from '@angular/router';
 import {ReportService} from './reports.service';
 
 @Component({
-  selector: 'reports',
-  template: `
+   template: `
     <table class="table table-bordered table-striped table-condensed">
       <thead>
-	<th>&nbsp;</th>
-	  <th *ngFor="let method of methods">{{method['id']}}</th>
+        <th>&nbsp;</th>
+          <th *ngFor="let method of methods">{{method['id']}}</th>
       </thead>
-    <tbody>
-      <tr *ngFor="let m_row of methods">
-        <th scope="row">{{m_row['id']}}</th>
-          <td *ngFor="let m_col of methods">
-	    <div *ngIf="m_row.id < m_col.id"
-	      (click)="onSelect(m_row, m_col)">
-	        {{m_row['id']}}/{{m_col['id']}}
-	    </div>
-	  </td>
-	</tr>
+      <tbody>
+        <tr *ngFor="let m_row of methods">
+          <th scope="row">{{m_row['id']}}</th>
+            <td *ngFor="let m_col of methods">
+              <div *ngIf="m_row.id < m_col.id"
+                (click)="onSelect(m_row, m_col)">
+                  {{m_row['id']}}/{{m_col['id']}}
+              </div>
+            </td>
+        </tr>
       </tbody>
-    </table>`,
+    </table>
+  `,
   providers: [ReportService],
 })
 
