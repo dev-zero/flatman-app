43897b2fd9c3dbf382dca4ba077c8462f4441022
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-05-23 15:07:05 +0200

    webapp: further cleanup the bootstrap file

--- fatman/app.ts
+++ fatman/app.ts
@@ -1,13 +1,19 @@
 
 import {bootstrap} from '@angular/platform-browser-dynamic';
+//import { enableProdMode } from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router';
-//import { enableProdMode } from '@angular/core';
-//enableProdMode();
 
 import {FatmanApp} from './app/fatman-app';
 
 import 'rxjs/Rx'
 
-bootstrap(FatmanApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
-  .catch(err => console.error(err));
+//enableProdMode();
+
+// Since services are simple classes, we have to specify providers here
+// instead of in some @Component section.
+bootstrap(FatmanApp, [
+         [HTTP_PROVIDERS, ROUTER_PROVIDERS],
+]);
+
+//  vim: set ts=4 sw=2 tw=0 :
