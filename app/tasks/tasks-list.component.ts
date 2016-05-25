f015b60966ce161a944a13347d162a6a05ddd66b
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-05-25 14:18:07 +0200

    webapp: use table style dictated by Bootstrap theme (default)

--- app/tasks/tasks-list.component.ts
+++ app/tasks/tasks-list.component.ts
@@ -4,27 +4,27 @@ import {TaskService} from './tasks.service';
 
 @Component({
   template: `
-  <div style="position: relative; left: 30px; /*border: 1px solid #73AD21; padding:5px; display: inline-block;*/">
-  <table  class="tasks">
+  <table class="tasks table table-bordered table-striped table-condensed">
     <thead>
-    <tr style="border-bottom: 2px solid; border-top: 2px solid;">
-        <th style="padding-right:20px"> <b>Id</b> </th>
-        <th style="padding-right:20px"> <b>Status</b> </th>
-        <th style="padding-right:20px"> <b>Test</b> </th>
-        <th style="padding-right:20px"> <b>Machine</b> </th>
-        <th style="padding-right:20px"> <b>Last update</b> </th>
-    </tr>
+      <tr>
+          <th> Id </th>
+          <th> Status </th>
+          <th> Test </th>
+          <th> Machine </th>
+          <th> Last update </th>
+      </tr>
     </thead>
     <tbody>
-        <tr *ngFor="let task of tasks" style="border-bottom: 1px solid;">
-            <td style="padding-right:20px"> {{task['id']}} </td>
-            <td style="padding-right:20px"> {{task['status']}} </td>
-            <td style="padding-right:20px"> {{task['structure']['name']}} </td>
-            <td style="padding-right:20px"> {{task['machine']}} </td>
-            <td style="padding-right:20px"> {{task['mtime']}} </td>
+        <tr *ngFor="let task of tasks">
+            <td> {{task.id}} </td>
+            <td> {{task.status}} </td>
+            <td> {{task.structure.name}} </td>
+            <td> {{task.machine}} </td>
+            <td> {{task.mtime}} </td>
         </tr>
     </tbody>
-  </table></div>`,
+  </table>
+  `,
   providers: [TaskService]
 })
 
