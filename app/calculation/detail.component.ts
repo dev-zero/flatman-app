041b1901240e477c9d131ad736cf1f9e7defba3f
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-12-15 19:25:52 +0100

    app: some more design work on calc tasks display

--- app/calculation/detail.component.ts
+++ app/calculation/detail.component.ts
@@ -10,15 +10,15 @@ import { CalculationsService } from './calculations.service';
 @Component({
   template: `
   <div class="row">
-    <div class="col-md-12" *ngIf="calculation">
+    <div class="col-md-12"
+      *ngIf="calculation">
+
       <h3>Calculation {{ calculation.id }}</h3>
       <p>
         <button (click)="gotoCalculations()">Back</button>
       </p>
 
-      <h4>Tasks</h4>
-
-      <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="panel-group" id="taskAccordion" role="tablist" aria-multiselectable="true">
         <div class="panel panel-default"
           *ngFor="let task of calculation.tasks"
           [ngClass]="{
@@ -26,15 +26,25 @@ import { CalculationsService } from './calculations.service';
             'panel-warning': task.status == 'running',
             'panel-success': task.status == 'done'
             }">
-          <div class="panel-heading">
-            Task {{ task.id }} <strong class="pull-right">{{ task.status | uppercase }}</strong>
+          <div class="panel-heading" role="tab"
+            [id]="'heading_' + task.id">
+            <h4 class="panel-title">
+              <a role="button" data-toggle="collapse" data-parent="#taskAccordion"
+                [href]="'#collapse_' + task.id"
+                [attr.aria-controls]="'collapse_' + task.id">
+                Task {{ task.id }} <span class="pull-right">{{ task.status | uppercase }}</span>
+              </a>
+            </h4>
           </div>
-          <div class="panel-body">
-            <dl class="dl-horizontal">
-              <dt>Last Update</dt><dd>{{ task.mtime }}</dd>
-              <dt>Created</dt><dd>{{ task.ctime }}</dd>
-              <dt>Machine</dt><dd>{{ task.machine }}</dd>
-            </dl>
+          <div class="panel-collapse collapse in" role="tabpanel" 
+              [id]="'collapse_' + task.id" [attr.aria-labelledby]="'heading_' + task.id">
+            <div class="panel-body">
+              <dl class="dl-horizontal">
+                <dt>Last Update</dt><dd>{{ task.mtime }}</dd>
+                <dt>Created</dt><dd>{{ task.ctime }}</dd>
+                <dt>Machine</dt><dd>{{ task.machine }}</dd>
+              </dl>
+            </div>
           </div>
         </div>
       </div>
